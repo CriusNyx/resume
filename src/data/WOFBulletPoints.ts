fb698b23984ee287595b10ea6d8f58b2e746665c
CriusNyx <rjreynoldsw@gmail.com>
2025-12-02 21:59:01 -0500

    Formatting fixes

--- src/data/WOFBulletPoints.ts
+++ src/data/WOFBulletPoints.ts
@@ -1,33 +1,33 @@
-export default `Managed a small team to develop and automate workflows to accomplish common business needs
-  Designed software that addressed common business needs
-  Converted pen and paper processes to digital ones
-  Added automatic drawing validation and scaling to digital floor plan drawings
+export default `Managed a small team to develop and automate workflows to accomplish common business needs.
+  Designed software that addressed common business needs.
+  Converted pen and paper processes to digital ones.
+  Added automatic drawing validation and scaling to digital floor plan drawings.
   Worked with management to design a quality user experience for sales team members.
-Created a RESTFUL web server using ASP.NET
-  Implemented endpoints using ASP and C#
-  Implemented endpoint schemas using GraphQL via Hot Chocolate
-  Implemented user authentication with Firebase
-  Designed database schema and documentation using Entity Relationship Diagrams, PlantUML, and Markdown
-  Interacted with database models using Entity Framework and SQL
-  Implemented database migrations using Entity Framework, and SQL
-  Using Postman and Banana Cake Pop to develop and test web server
-  Implemented automated testing using XUnit
-  Added field level data authorization to protect trade secrets
-Created the World of Floors Web App for managing projects
-  Implemented web frontend using React Dom and Material UI
-  Integrated web application into API using Axios, Redux, and Redux Thunk
-  Created feature for storing, retrieving, and editing customer information
-  Implement web sockets to allow for real time synchronization
-  Created an ordering system to automatically generate vendor orders
-  Created a pick sheet feature to assist warehouse workers in gathering materials for jobs
-  Created an inventory management system for tracking, auditing, and updating inventory information
-Created the World of Floors Mobile App using Xamarin .NET
-  Added support for Windows, Android, and iOS
-  Created, prototyped, and implemented a user-friendly experience for creating technical drawings
-  Created customized workflows for different teams with different needs
-  Implemented algorithms to automatically compute job material requirements
-  Implemented a quote presentation system, allowing sales people to present quotes to customers
-  Implemented an undo/redo system to accommodate for user errors
-  Added support for external bluetooth measuring devices
-  Reverse engineered BLE API for Leica Disto laser measuring devices
-Deployed application stack using AWS, EC2 and Elastic Beanstalk`;
+Created a RESTFUL web server using ASP.NET.
+  Implemented endpoints using ASP and C#.
+  Implemented endpoint schemas using GraphQL via Hot Chocolate.
+  Implemented user authentication with Firebase.
+  Designed database schema and documentation using Entity Relationship Diagrams, PlantUML, and Markdown.
+  Interacted with database models using Entity Framework and SQL.
+  Implemented database migrations using Entity Framework, and SQL.
+  Using Postman and Banana Cake Pop to develop and test web server.
+  Implemented automated testing using XUnit.
+  Added field level data authorization to protect trade secrets.
+Created the World of Floors Web App for managing projects.
+  Implemented web frontend using React Dom and Material UI.
+  Integrated web application into API using Axios, Redux, and Redux Thunk.
+  Created feature for storing, retrieving, and editing customer information.
+  Implement web sockets to allow for real time synchronization.
+  Created an ordering system to automatically generate vendor orders.
+  Created a pick sheet feature to assist warehouse workers in gathering materials for jobs.
+  Created an inventory management system for tracking, auditing, and updating inventory information.
+Created the World of Floors Mobile App using Xamarin .NET.
+  Added support for Windows, Android, and iOS.
+  Created, prototyped, and implemented a user-friendly experience for creating technical drawings.
+  Created customized workflows for different teams with different needs.
+  Implemented algorithms to automatically compute job material requirements.
+  Implemented a quote presentation system, allowing sales people to present quotes to customers.
+  Implemented an undo/redo system to accommodate for user errors.
+  Added support for external bluetooth measuring devices.
+  Reverse engineered BLE API for Leica Disto laser measuring devices.
+Deployed application stack using AWS, EC2 and Elastic Beanstalk.`;
