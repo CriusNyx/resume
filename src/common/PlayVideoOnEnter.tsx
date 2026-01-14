98fba81eb1ff74aff8ab9cf79c8bf6c4f6735e65
CriusNyx <rjreynoldsw@gmail.com>
2026-01-14 17:42:08 -0500

    Added play button for no input

--- src/common/PlayVideoOnEnter.tsx
+++ src/common/PlayVideoOnEnter.tsx
@@ -1,6 +1,6 @@
 import { useInView } from "motion/react";
-import { useEffect, useRef } from "react";
-import { CornerLeftUp } from "react-feather";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { CornerLeftUp, Play } from "react-feather";
 
 export interface PlayVideoOnEnterProps {
   src: string;
@@ -11,16 +11,34 @@ export function PlayVideoOnEnter(props: PlayVideoOnEnterProps) {
   const inViewRef = useRef(null);
   const videoRef = useRef<HTMLVideoElement>(null);
   const inView = useInView(inViewRef, { once: true, amount: 0.75 });
+  const [showPlay, setShowPlay] = useState(false);
+
+  const play = useCallback(() => {
+    videoRef.current?.play().then(() => setShowPlay(false)).catch(() =>
+      setShowPlay(true)
+    );
+  }, []);
 
   useEffect(() => {
     if (inView) {
-      videoRef.current?.play();
+      play();
     }
-  }, [inView]);
+  }, [inView, play]);
 
   return (
     <div className="flex flex-col items-center" ref={inViewRef}>
-      <video ref={videoRef} src={props.src} />
+      <div
+        className="relative"
+        style={{ cursor: showPlay ? "pointer" : undefined }}
+        onClick={play}
+      >
+        <video ref={videoRef} src={props.src} />
+        {showPlay && (
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+            <Play size={64} />
+          </div>
+        )}
+      </div>
       {props.blenderBadgeText && (
         <p className="text-red-400 flex flex-row items-center shadow-2xl">
           <CornerLeftUp size={14} /> &nbsp; {props.blenderBadgeText}
